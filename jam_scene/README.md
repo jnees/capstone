5220f40243010f8da2fbd13a774d016c8f3069d0
Joshua Nees <joshua.nees@gmail.com>
2022-06-28 22:16:27 -0700

    Initialize Flutter

---
+++ jam_scene/README.md
@@ -0,0 +1,1 @@
+# jam_scene UI
---
+++ jam_scene/lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'JamScene',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: const HomePage());
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: const Text("JamScene")),
+        body: const Center(child: Text("Welcome!")));
+  }
+}
