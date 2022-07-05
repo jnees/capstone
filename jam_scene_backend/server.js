eb6cff8e05668cbb0cb439debd43382348a8f070
wrmacdonald <wrmacdonald2016@gmail.com>
2022-07-05 16:05:16 -0700

    PostgreSQL connection & first route setup

--- jam_scene_backend/server.js
+++ jam_scene_backend/server.js
@@ -1,16 +1,19 @@
 const express = require("express");
 
 const app = express();
+const db = require("./db/index.js");
 
 // Add req.body to all requests
 app.use(express.json());
-app.use(express.urlencoded({ extended: true}));
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/users", db.getUsers);
 
 app.get("/", (req, res) => {
-    res.send("Hello World!")
+  res.send("Hello World!");
 });
 
 const port = 8000;
 app.listen(port, () => {
-    console.log(`Listening on post: ${port}`);
+  console.log(`Listening on post: ${port}`);
 });
