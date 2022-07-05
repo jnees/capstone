d9ef572cb969fb1b536b73442c64113625c03296
Solange Coughlin <coughlis@oregonstate.edu>
2022-07-05 11:49:54 -0600

    modified hello world route to remove json obj

--- jam_scene_backend/server.js
+++ jam_scene_backend/server.js
@@ -7,7 +7,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
 
 app.get("/", (req, res) => {
-    res.send({msg: "Hello World!"})
+    res.send("Hello World!")
 });
 
 const port = 8000;
