1c32dfb0e0963501adf39ae7c71228c26604348d
Solange Coughlin <coughlis@oregonstate.edu>
2022-07-06 14:41:22 -0600

    testing lint workflow

--- jam_scene_backend/app.test.js
+++ jam_scene_backend/app.test.js
@@ -2,6 +2,14 @@
 const request = require("supertest");
 const app = require("./app.js");
 
+/*
+Testing server API
+
+Reference:
+Method based on Youtube tutorial from Sam Meech-Ward:
+https://youtu.be/FKnzS_icp20
+*/
+
 describe("GET /", () => {
   test("should respond with > 200 status code", async () => {
     const response = await request(app).get("/");
