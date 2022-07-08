2e14e7a748e67bb6c9f1377555256f6b563a2e8b
Solange Coughlin <coughlis@oregonstate.edu>
2022-07-08 16:53:32 -0600

    fixing conflicts in db:

--- jam_scene_backend/db/db_pools.js
+++ jam_scene_backend/db/db_pools.js
@@ -1,28 +1,12 @@
 require("dotenv").config();
 const { Pool } = require("pg");
 
-// const pool = new Pool({
-//   connectionString: process.env.DATABASE_URL,
-//   ssl:
-//   {
-//     rejectUnauthorized: false
-//   }
-// });
-
-const pool = (() => {
-  if (process.env.NODE_ENV !== "production") {
-    return new Pool({
-      connectionString: process.env.DATABASE_URL,
-      ssl: false
-    });
-  } else {
-    return new Pool({
-      connectionString: process.env.DATABASE_URL,
-      ssl: {
-        rejectUnauthorized: false
-      }
-    });
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl:
+  {
+    rejectUnauthorized: false
   }
-})();
+});
 
 module.exports = pool;
