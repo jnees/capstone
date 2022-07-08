40cdce5caf10eea2978b0f3fe845c9b0a1c3986b
Solange Coughlin <coughlis@oregonstate.edu>
2022-07-08 15:39:34 -0600

    Revert "Merge pull request #14 from jnees/db-connection"
    This reverts commit be201df45420696ae13371c42fccb80c62841e64, reversing
    changes made to 5c1a29e362c5a8b7d9b8feb5a4766ed96b61d74f.

--- jam_scene_backend/app.js
+++ jam_scene_backend/app.js
@@ -1,46 +1,12 @@
 const express = require("express");
 const app = express();
 const db = require("./db/index.js");
-require("dotenv").config();
-const pool = require("./db/db_pools.js");
 
 // Add req.body to all requests
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/users", db.getUsers);
-
-/*eslint-disable no-unused-vars*/
-app.get("/make-table", (req, res) => {
-  const makeUsers = "CREATE TABLE IF NOT EXISTS users \
-    id SERIAL PRIMARY KEY,\
-    email text NOT NULL,\
-    first_name text,\
-    last_name text,\
-    location text,\
-    influences text,\
-    recordings text,\
-    description text,\
-    join_date timestamp,\
-    instruments integer REFERENCES instruments,\
-    availability_mon_am boolean, \
-    availability_mon_pm boolean, \
-    availability_tue_am boolean, \
-    availability_tue_pm boolean, \
-    availability_wed_am boolean, \
-    availability_wed_pm boolean, \
-    availability_thu_am boolean, \
-    availability_thu_pm boolean, \
-    availability_fri_am boolean, \
-    availability_fri_pm boolean, \
-    availability_sat_am boolean, \
-    availability_sat_pm boolean, \
-    availability_sun_am boolean, \
-    availability_sun_pm boolean, \
-    profile_photo text";
-
-  pool.query(makeUsers).then(console.log("did query")).catch((err) => console.log(err));
-});
 
 app.get("/", (req, res) => {
   res.send("Hello World!!!");
