35a2175535e628b3451d5894ec4f628aeebbcf45
Joshua Nees <joshua.nees@gmail.com>
2022-07-11 16:08:20 -0700

    Fix web google sign in and add layout builder for profile.

--- jam_scene_UI/lib/screens/profile_page.dart
+++ jam_scene_UI/lib/screens/profile_page.dart
@@ -1,9 +1,16 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
-class ProfilePage extends StatelessWidget {
+class ProfilePage extends StatefulWidget {
   const ProfilePage({Key? key}) : super(key: key);
 
+  static const routeName = '/profile';
+
+  @override
+  State<ProfilePage> createState() => _ProfilePageState();
+}
+
+class _ProfilePageState extends State<ProfilePage> {
   void signOut() {
     FirebaseAuth.instance.signOut();
   }
@@ -12,13 +19,50 @@ class ProfilePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(title: const Text("JamScene")),
-        body: Center(
-            child: Column(
-          children: [
-            Text("${FirebaseAuth.instance.currentUser}"),
-            ElevatedButton(
-                onPressed: (() => (signOut())), child: const Text("Sign Out"))
-          ],
-        )));
+        body: LayoutBuilder(
+          builder: (context, constraints) {
+            if (constraints.maxWidth > 600) {
+              return const WebProfilePage();
+            } else {
+              return const MobileProfilePage();
+            }
+          },
+        ));
+  }
+}
+
+class MobileProfilePage extends StatelessWidget {
+  const MobileProfilePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        children: [
+          Text("${FirebaseAuth.instance.currentUser}"),
+          ElevatedButton(
+              onPressed: (() => (FirebaseAuth.instance.signOut())),
+              child: const Text("Sign Out"))
+        ],
+      ),
+    );
+  }
+}
+
+class WebProfilePage extends StatelessWidget {
+  const WebProfilePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        children: [
+          Text("${FirebaseAuth.instance.currentUser}"),
+          ElevatedButton(
+              onPressed: (() => (FirebaseAuth.instance.signOut())),
+              child: const Text("Sign Out"))
+        ],
+      ),
+    );
   }
 }
