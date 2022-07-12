f9d15e308ac3a6c8d3643ebe803859cdf8b390a0
Joshua Nees <joshua.nees@gmail.com>
2022-07-12 10:40:41 -0700

    Add Navigation bars to mobile/web profile

--- jam_scene_UI/lib/screens/profile_page.dart
+++ jam_scene_UI/lib/screens/profile_page.dart
@@ -17,44 +17,108 @@ class _ProfilePageState extends State<ProfilePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(title: const Text("JamScene")),
-        body: LayoutBuilder(
-          builder: (context, constraints) {
-            if (constraints.maxWidth > 600) {
-              return const WebProfilePage();
-            } else {
-              return const MobileProfilePage();
-            }
-          },
-        ));
-  }
-}
-
-class MobileProfilePage extends StatelessWidget {
-  const MobileProfilePage({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        children: [
-          Text("${FirebaseAuth.instance.currentUser}"),
-          ElevatedButton(
-              onPressed: (() => (FirebaseAuth.instance.signOut())),
-              child: const Text("Sign Out"))
-        ],
-      ),
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        if (constraints.maxWidth > 600) {
+          return const WebProfilePage();
+        } else {
+          return const MobileProfilePage();
+        }
+      },
     );
   }
 }
 
-class WebProfilePage extends StatelessWidget {
+class MobileProfilePage extends StatefulWidget {
+  const MobileProfilePage({Key? key}) : super(key: key);
+
+  @override
+  State<MobileProfilePage> createState() => _MobileProfilePageState();
+}
+
+class _MobileProfilePageState extends State<MobileProfilePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("JamScene"),
+      ),
+      body: const Text("Mobile Profile Page"),
+      bottomNavigationBar: BottomNavigationBar(
+          type: BottomNavigationBarType.fixed,
+          items: const [
+            BottomNavigationBarItem(icon: Icon(Icons.person), label: "Profile"),
+            BottomNavigationBarItem(icon: Icon(Icons.search), label: "Search"),
+            BottomNavigationBarItem(icon: Icon(Icons.newspaper), label: "Ads"),
+            BottomNavigationBarItem(icon: Icon(Icons.mail), label: "Messages"),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.settings), label: "Settings"),
+          ]),
+    );
+  }
+}
+
+class WebProfilePage extends StatefulWidget {
   const WebProfilePage({Key? key}) : super(key: key);
 
   @override
+  State<WebProfilePage> createState() => _WebProfilePageState();
+}
+
+class _WebProfilePageState extends State<WebProfilePage> {
+  void signOut() {
+    FirebaseAuth.instance.signOut();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Center(
+    return Scaffold(
+      body: Row(children: [
+        NavigationRail(
+          selectedIndex: 0,
+          onDestinationSelected: (index) {},
+          destinations: const [
+            NavigationRailDestination(
+              icon: Tooltip(message: "Profile", child: Icon(Icons.person)),
+              label: Text("Profile"),
+            ),
+            NavigationRailDestination(
+              icon: Tooltip(
+                  message: "Search Profiles", child: Icon(Icons.search)),
+              label: Text("Search"),
+            ),
+            NavigationRailDestination(
+              icon: Tooltip(message: "Ads", child: Icon(Icons.newspaper)),
+              label: Text("Ads"),
+            ),
+            NavigationRailDestination(
+              icon: Tooltip(message: "Messages", child: Icon(Icons.mail)),
+              label: Text("Messages"),
+            ),
+            NavigationRailDestination(
+              icon: Tooltip(message: "Settings", child: Icon(Icons.settings)),
+              label: Text("Settings"),
+            ),
+            NavigationRailDestination(
+              icon:
+                  Tooltip(message: "Sign out", child: Icon(Icons.exit_to_app)),
+              label: Text("Sign Out"),
+            ),
+          ],
+        ),
+        const VerticalDivider(thickness: 1, width: 1),
+        const MainContent()
+      ]),
+    );
+  }
+}
+
+class MainContent extends StatelessWidget {
+  const MainContent({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
       child: Column(
         children: [
           Text("${FirebaseAuth.instance.currentUser}"),
