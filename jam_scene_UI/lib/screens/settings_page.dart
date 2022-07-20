18ca41e67600801b449d46d22190b8cf5390e417
Joshua Nees <joshua.nees@gmail.com>
2022-07-19 22:28:09 -0700

    Add account delete feature to settings page.

--- jam_scene_UI/lib/screens/settings_page.dart
+++ jam_scene_UI/lib/screens/settings_page.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class SettingsPage extends StatefulWidget {
   const SettingsPage({Key? key}) : super(key: key);
@@ -15,10 +16,54 @@ class _SettingsPageState extends State<SettingsPage> {
     }
   }
 
+  void _deleteAccount() async {
+    Navigator.pop(context, 'Delete');
+    var token = await FirebaseAuth.instance.currentUser!.getIdToken();
+    var uid = FirebaseAuth.instance.currentUser!.uid;
+    var url = Uri.parse('https://jam-scene-app.herokuapp.com/user/$uid');
+    var response = await http.delete(url, headers: {'Authorization': token});
+    if (response.statusCode == 200) {
+      _logOut();
+    } else {
+      debugPrint("Error deleting account");
+    }
+  }
+
+  Widget deleteWarning() {
+    return AlertDialog(
+      elevation: 24,
+      title: const Text('Delete Account?'),
+      content: const Text('This is action is permanent cannot be undone.'),
+      actions: [
+        TextButton(
+          onPressed: () => Navigator.pop(context, 'Cancel'),
+          child: const Text('Cancel'),
+        ),
+        TextButton(
+          onPressed: _deleteAccount,
+          child: const Text('Delete'),
+        ),
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: ElevatedButton(onPressed: _logOut, child: const Text("Log Out")),
+      child: Column(
+        children: [
+          ElevatedButton(onPressed: _logOut, child: const Text("Log Out")),
+          ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                primary: Colors.red,
+              ),
+              onPressed: () => showDialog(
+                  builder: (_) => deleteWarning(),
+                  context: context,
+                  barrierDismissible: true),
+              child: const Text("Delete Account")),
+        ],
+      ),
     );
   }
 }
