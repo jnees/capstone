1c31b7f11687640e490b31db1d48b279f6e699b8
Joshua Nees <joshua.nees@gmail.com>
2022-08-04 21:06:43 -0700

    GestureDetector updates around all forms with numeric imput.

--- jam_scene_UI/lib/screens/ad_search_page.dart
+++ jam_scene_UI/lib/screens/ad_search_page.dart
@@ -54,108 +54,111 @@ class _AdSearchState extends State<AdSearch> {
         ),
         Expanded(
           child: SingleChildScrollView(
-            child: Form(
-              key: adSearchFormKey,
-              child: Column(children: [
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: const [
-                      Padding(
-                        padding: EdgeInsets.all(8.0),
-                        child: Text("Filter",
-                            style: TextStyle(fontWeight: FontWeight.bold)),
+            child: GestureDetector(
+              onTap: () => FocusScope.of(context).unfocus(),
+              child: Form(
+                key: adSearchFormKey,
+                child: Column(children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: const [
+                        Padding(
+                          padding: EdgeInsets.all(8.0),
+                          child: Text("Filter",
+                              style: TextStyle(fontWeight: FontWeight.bold)),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Row(
+                      children: [
+                        const Text("Instrument"),
+                        const Spacer(),
+                        DropdownButton(
+                            value: instrument,
+                            onChanged: (selectedValue) {
+                              if (selectedValue is String) {
+                                setState(() {
+                                  instrument = selectedValue;
+                                });
+                              }
+                            },
+                            items: [
+                              for (MapEntry e in instrumentLookup.entries)
+                                DropdownMenuItem(
+                                  value: e.value,
+                                  child: Text(e.value),
+                                ),
+                            ]),
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: TextFormField(
+                      decoration: const InputDecoration(
+                        labelText: "City",
+                        border: OutlineInputBorder(),
                       ),
-                    ],
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Row(
-                    children: [
-                      const Text("Instrument"),
-                      const Spacer(),
-                      DropdownButton(
-                          value: instrument,
-                          onChanged: (selectedValue) {
-                            if (selectedValue is String) {
-                              setState(() {
-                                instrument = selectedValue;
-                              });
-                            }
-                          },
-                          items: [
-                            for (MapEntry e in instrumentLookup.entries)
-                              DropdownMenuItem(
-                                value: e.value,
-                                child: Text(e.value),
-                              ),
-                          ]),
-                    ],
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: TextFormField(
-                    decoration: const InputDecoration(
-                      labelText: "City",
-                      border: OutlineInputBorder(),
-                    ),
-                    controller: _cityController,
-                    validator: (value) {
-                      if (value!.isEmpty) {
-                        return "Please enter a city";
-                      }
-                      return null;
-                    },
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: TextFormField(
-                    decoration: const InputDecoration(
-                      labelText: "State Abbrev.",
-                      border: OutlineInputBorder(),
-                    ),
-                    controller: _stateController,
-                    validator: (value) {
-                      if (value!.isEmpty) {
-                        return "Please enter a state abbreviation";
-                      } else if (value.length != 2) {
-                        return "Please enter a valid abbreviation, like 'CA' or 'WA'.";
-                      }
-                      return null;
-                    },
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: TextFormField(
-                    decoration: const InputDecoration(
-                      labelText: "Near zipcode",
-                      hintText: "90210",
-                      border: OutlineInputBorder(),
-                    ),
-                    controller: _zipController,
-                    keyboardType: TextInputType.number,
-                    validator: (value) {
-                      if (value!.isEmpty) {
-                        return "Please enter a zipcode";
-                      }
-                      return null;
-                    },
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(12.0),
-                  child: ElevatedButton(
-                      onPressed: () {
-                        _filterSearchResults();
+                      controller: _cityController,
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return "Please enter a city";
+                        }
+                        return null;
                       },
-                      child: const Text("Apply Filter")),
-                ),
-              ]),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: TextFormField(
+                      decoration: const InputDecoration(
+                        labelText: "State Abbrev.",
+                        border: OutlineInputBorder(),
+                      ),
+                      controller: _stateController,
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return "Please enter a state abbreviation";
+                        } else if (value.length != 2) {
+                          return "Please enter a valid abbreviation, like 'CA' or 'WA'.";
+                        }
+                        return null;
+                      },
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: TextFormField(
+                      decoration: const InputDecoration(
+                        labelText: "Near zipcode",
+                        hintText: "90210",
+                        border: OutlineInputBorder(),
+                      ),
+                      controller: _zipController,
+                      keyboardType: TextInputType.number,
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return "Please enter a zipcode";
+                        }
+                        return null;
+                      },
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(12.0),
+                    child: ElevatedButton(
+                        onPressed: () {
+                          _filterSearchResults();
+                        },
+                        child: const Text("Apply Filter")),
+                  ),
+                ]),
+              ),
             ),
           ),
         )
